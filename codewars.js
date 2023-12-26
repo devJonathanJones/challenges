d522850ddac42ef61596256a68ea54664da5ae50
Jonathan Jones <devjonathanjones@gmail.com>
2023-12-26 09:22:46 -0600

    completed 6kyu kata.

--- codewars.js
+++ codewars.js
@@ -104,3 +104,13 @@ function openOrSenior(data){
 
 
 
+function findNextSquare(sq) {
+  if (Math.sqrt(sq) % 1 != 0) {
+    return -1
+  } else {
+    return (Math.sqrt(sq) + 1) * (Math.sqrt(sq) + 1)
+  }
+}
+
+
+
