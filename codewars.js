d5d690db59e9a7bcac666bf56cb5e23ef28b9b78
Jonathan Jones <devjonathanjones@gmail.com>
2024-01-01 08:38:29 -0600

    completed 7kyu kata

--- codewars.js
+++ codewars.js
@@ -174,3 +174,17 @@ function printerError(s) {
 
 
 
+function isTriangle (a, b, c) {
+  if (a + b <= c) {
+    return false
+  } else if (b + c <= a) {
+    return false
+  } else if (a + c <= b) {
+    return false
+  } else {
+    return true
+  }
+}
+
+
+
