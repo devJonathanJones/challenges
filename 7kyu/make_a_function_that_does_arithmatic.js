63e037393b6da05a3539ecd4890f6651f371473e
Jonathan Jones <devjonathanjones@gmail.com>
2024-01-03 20:41:00 -0600

    completed 7kyu kata

---
+++ 7kyu/make_a_function_that_does_arithmatic.js
@@ -0,0 +1,16 @@
+function arithmetic(a, b, operator){
+    switch (operator) {
+        case 'add':
+          return a + b
+          break
+        case 'subtract':
+          return a - b
+          break
+        case 'multiply':
+          return a * b
+          break
+        case 'divide':
+          return a / b
+          break
+    }
+  }
