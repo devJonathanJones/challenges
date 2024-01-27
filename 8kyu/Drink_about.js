666b60a503cded3bed1b2e828abcdcf694eef002
Jonathan Jones <devjonathanjones@gmail.com>
2024-01-27 08:13:51 -0600

    completed 8kyu kata

---
+++ 8kyu/Drink_about.js
@@ -0,0 +1,11 @@
+function peopleWithAgeDrink(age) {
+    if (age >= 21) {
+      return "drink whisky"
+    } else if (age >= 18) {
+      return "drink beer"
+    } else if (age >= 14) {
+      return "drink coke"
+    } else {
+      return "drink toddy"
+    }
+  }
